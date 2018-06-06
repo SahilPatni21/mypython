e740150e98fe560c0bf5f666d44dc593bd0b90d2
root <root@localhost.localdomain>
2018-06-06 13:04:11 +0530

    for voice output

---
+++ Socket_Programming/sender.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2
+
+#import modeule
+import socket
+
+# to make connection between sender and receiver
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+'''
+socket --- ip(4Byte)+port(2bytes) -- 6bytes
+'''
+
+
+
+for i in range(100000):
+	     msg=raw_input("enter your msg :  ")
+	     s.sendto(msg,("127.0.0.1",7777))			#To send message to receiver
+	     s.recvfrom(1000)					#To print receive message in sender side
